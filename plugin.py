f1994730cb298a64a400ef8c6ff8edf16520391b
DerekGn <dg89327@hotmail.com>
2020-09-17 17:57:15 +0100

    Perform device refresh

--- plugin.py
+++ plugin.py
@@ -88,9 +88,11 @@ class BasePlugin:
         if(len(Devices) < len(homeAddresses) * 5):
             Domoticz.Log("Creating Devices")
             for x in range(1, (len(homeAddresses) * 5) + 1):
-                Domoticz.Device(Name="Home Switch"+str(x), Unit=x, Type=244, Subtype=62, Switchtype=0).Create()
-
+                Domoticz.Device(Name="Home Switch"+str(x), Unit=x, TypeName="Switch", Type=244, Subtype=62, Switchtype=0).Create()
             Domoticz.Log("Created "+str(len(Devices))+" Devices")
+
+        for Device in Devices:
+            Devices[Device].Update(nValue=Devices[Device].nValue, sValue=Devices[Device].sValue)
 
         SerialConn = Domoticz.Connection(Name="Serial Connection", Transport="Serial", Protocol="None", Address=Parameters["SerialPort"], Baud=115200)
         SerialConn.Connect()
